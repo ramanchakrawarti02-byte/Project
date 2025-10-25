dc6795953a643eb39f2e5e350daf89edce57ac09
ramanchakrawarti02-byte <ramanchakrawarti02@gmail.com>
2025-10-25 18:06:03 +0530

    Update Quizgame.py

--- Quizgame.py
+++ Quizgame.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.write("Hello welcome to Quiz Game application he")
- st.sidebar.markdown("Quiz Menu...")
+st.sidebar.markdown("Quiz Menu...")
 st.sidebar.markdown("Quiz Game")
 st.balloons()
 n = st.checkbox("A")
