a7b1625d0b40b9c608670ac6ae74f36365972035
ramanchakrawarti02-byte <ramanchakrawarti02@gmail.com>
2026-03-01 12:59:51 +0530

    Update Quizgame.py

--- Quizgame.py
+++ Quizgame.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
-#st.write("Hello welcome to Quiz Game application he")
-#st.sidebar.markdown("Quiz Menu...")
-#st.sidebar.markdown("Quiz Game")
-#st.balloons()
-#n = st.checkbox("A")
-#n = st.checkbox("B")
-#n = st.text_input("Enter,,,,",placeholder="Please press enter to feed answer")
-#st.write(n)
+st.write("Hello welcome to Quiz Game application he")
+st.sidebar.markdown("Quiz Menu...")
+st.sidebar.markdown("Quiz Game")
+st.balloons()
+n = st.checkbox("A")
+n = st.checkbox("B")
+n = st.text_input("Enter,,,,",placeholder="Please press enter to feed answer")
+st.write(n)
 
