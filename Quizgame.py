77d5f0c8ee188a8ef300783435cf52ef1b78fee2
ramanchakrawarti02-byte <ramanchakrawarti02@gmail.com>
2026-03-08 12:15:16 +0530

    Update Quizgame.py

--- Quizgame.py
+++ Quizgame.py
@@ -8,26 +8,18 @@ st.write("Q1. Which is the first alphabet of english language?\nA) c   B) d    C
 ans = st.text_input("Enter your choice...")
 if ans == 'c' or ans=='C':
   score+=5
-else:
-  score = 0
 st.write("Q2. Which language is widely used for data analysis?\nA)HTML    B) CSS   C) JAVA   D) PYHTONE ")
 ans = st.text_input("Enter your choice...")
 if ans == 'd' or ans=='D':
   score+=5
-else:
-  score = 0 
 st.write("Q3.What does SQL stand for?\nA)Simple Query Language   B) system Query language   C) structure Query lamguage   D) Sequential Query Language ")
 ans = st.text_input("Enter your choice...")
 if ans == 'c' or ans=='C':
   score+=5
-else:
-  score = 0 
 st.write("Q4.Which chart is best for showing trends over time?\nA)pie chat  B) line chat   C) bar chat   D) Scatter Plot ")
 ans = st.text_input("Enter your choice...")
 if ans == 'c' or ans=='C':
   score+=5
-else:
-  score = 0      
 if score == 20 :
   st.write("congratulations! you have scored",score,'in quiz')
   st.balloons()
