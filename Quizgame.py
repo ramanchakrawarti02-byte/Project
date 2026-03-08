0acff4637cdf920b0f7d5c91bc5c4b26b670b55a
ramanchakrawarti02-byte <ramanchakrawarti02@gmail.com>
2026-03-08 11:48:02 +0530

    Update Quizgame.py

--- Quizgame.py
+++ Quizgame.py
@@ -10,9 +10,34 @@ if ans == 'c' or ans=='C':
   score=5
 else:
   score = 0
-if score == 5 :
+st.write("Q2. Which language is widely used for data analysis?\nA)HTML    B) CSS   C) JAVA   D) PYHTONE ")
+ans = st.text_input("Enter your choice...",placeholder="Please press enter to feed answer")
+if ans == 'd' or ans=='D':
+  score=5
+else:
+  score = 0 
+st.write("Q3.What does SQL stand for?\nA)Simple Query Language   B) system Query language   C) structure Query lamguage   D) Sequential Query Language ")
+ans = st.text_input("Enter your choice...",placeholder="Please press enter to feed answer")
+if ans == 'c' or ans=='C':
+  score=5
+else:
+  score = 0 
+st.write("Q4.Which chart is best for showing trends over time?\nA)pie chat  B) line chat   C) bar chat   D) Scatter Plot ")
+ans = st.text_input("Enter your choice...",placeholder="Please press enter to feed answer")
+if ans == 'c' or ans=='C':
+  score=5
+else:
+  score = 0    
+  
+if score == 20 :
   st.write("congratulations! you have scored",score,'in quiz')
   st.balloons()
+elif score == 15 :
+   st.write("congratulations! you have scored",score,'in quiz')
+   st.snow() 
+else:
+  st.write("fail")
+  
 # n = st.checkbox("A")
 # n = st.checkbox("B")
 # n = st.text_input("Enter,,,,",placeholder="Please press enter to feed answer")
