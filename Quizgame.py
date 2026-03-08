4cdacc82fd8c31d645a387ff386c14c47d37d517
ramanchakrawarti02-byte <ramanchakrawarti02@gmail.com>
2026-03-08 12:03:14 +0530

    Update Quizgame.py

--- Quizgame.py
+++ Quizgame.py
@@ -5,25 +5,25 @@ st.sidebar.markdown("Quiz Menu...")
 st.sidebar.markdown("Quiz Game")
 score = 0
 st.write("Q1. Which is the first alphabet of english language?\nA) c   B) d    C) a   D) f")
-ans = st.text_input("Enter your choice...",placeholder="Please press enter to feed answer")
+ans = st.text_input("Enter your choice...")
 if ans == 'c' or ans=='C':
   score=5
 else:
   score = 0
 st.write("Q2. Which language is widely used for data analysis?\nA)HTML    B) CSS   C) JAVA   D) PYHTONE ")
-ans2 = st.text_input("Enter your choice...",placeholder="Please press enter to feed answer")
+ans2 = st.text_input("Enter your choice...")
 if ans2 == 'd' or ans=='D':
   score=5
 else:
   score = 0 
 st.write("Q3.What does SQL stand for?\nA)Simple Query Language   B) system Query language   C) structure Query lamguage   D) Sequential Query Language ")
-ans3 = st.text_input("Enter your choice...",placeholder="Please press enter to feed answer")
+ans3 = st.text_input("Enter your choice...")
 if ans3 == 'c' or ans=='C':
   score=5
 else:
   score = 0 
 st.write("Q4.Which chart is best for showing trends over time?\nA)pie chat  B) line chat   C) bar chat   D) Scatter Plot ")
-ans4 = st.text_input("Enter your choice...",placeholder="Please press enter to feed answer")
+ans4 = st.text_input("Enter your choice...")
 if ans4 == 'c' or ans=='C':
   score=5
 else:
